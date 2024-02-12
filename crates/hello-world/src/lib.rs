5eea3a1a9e6956d9f0ea5acc565870d9a44121db
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-02-11 20:50:20 -0800

    fn should take arguments
    for now it will be a selector

--- crates/hello-world/src/lib.rs
+++ crates/hello-world/src/lib.rs
@@ -1,9 +1,9 @@
 use reqwest::blocking;
 use scraper::{selector, Html, Selector};
 
-pub fn fetch_wiki() -> Result<String, Box<dyn std::error::Error>> {
     let response = blocking::get("https://en.wikipedia.org/wiki/Battle_of_the_Bulge");
     let html_content = response?.text()?;
+pub async fn fetch_wiki<'a>(myselector: &'a str) -> Result<String, Box<dyn std::error::Error + 'a>> {
 
     // let's use a parser
     let document = Html::parse_document(&html_content);
