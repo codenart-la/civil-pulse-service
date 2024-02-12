d82105265f06b51ba0ed615f1ef1c1e36eb843d2
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-02-11 20:50:50 -0800

    remove unnecessary question mark

--- crates/hello-world/src/lib.rs
+++ crates/hello-world/src/lib.rs
@@ -1,9 +1,9 @@
 use reqwest::blocking;
 use scraper::{selector, Html, Selector};
 
-    let html_content = response?.text()?;
 pub async fn fetch_wiki<'a>(myselector: &'a str) -> Result<String, Box<dyn std::error::Error + 'a>> {
     let response = reqwest::get("https://en.wikipedia.org/wiki/Battle_of_the_Bulge").await;
+    let html_content = response?.text();
 
     // let's use a parser
     let document = Html::parse_document(&html_content);
